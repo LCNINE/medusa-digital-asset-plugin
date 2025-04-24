d8f6a0975cd4924dc806960616755b3229240485
Pear-HyunJI <ponyo3764@gmail.com>
2025-04-24 13:26:53 +0900

    temp:임시

--- src/api/store/library/[id]/download/route.ts
+++ src/api/store/library/[id]/download/route.ts
@@ -29,23 +29,24 @@ export async function GET(req: AuthenticatedMedusaRequest, res: MedusaResponse) 
         throw new MedusaError(MedusaError.Types.NOT_FOUND, "소유권 먼저 행사해야 다운로드 할 수 있습니다.")
     }
 
+    // TODO: 추후 여기 console.log(license) 찍어보기,
     // FILE Module dptj Signed URL 임시 싸용
     // AWS S3를 아직 연동하지 않아 presigned URL 기능을 바로 사용할 수 없습니다.
     // 그래서 지금은 fileService.retrieveFile()로 반환되는 fileRecord.url 을 내려주고,
     // 차후 AWS 연결 완료 후 getPresignedDownloadUrl() 으로 교체할 예정입니다.
-    const fileService = req.scope.resolve(Modules.FILE)
-    const fileKey = license.digital_asset.file_url
-    const fileRecord   = await fileService.retrieveFile(fileKey)
+    // const fileService = req.scope.resolve(Modules.FILE)
+    // const fileKey = license.digital_asset.file_url
+    // const fileRecord   = await fileService.retrieveFile(fileKey)
 
-    if (!fileRecord.url) {
-        throw new MedusaError(
-        MedusaError.Types.NOT_FOUND,
-        "파일 URL 정보를 찾을 수 없습니다."
-        )
-    }
+    // if (!fileRecord.url) {
+    //     throw new MedusaError(
+    //     MedusaError.Types.NOT_FOUND,
+    //     "파일 URL 정보를 찾을 수 없습니다."
+    //     )
+    // }
     
     //  지금방식은 로컬 파일 경로나 CDN 경로가 그대로 노출돼서
     // 추후 AWS S3 버킷 연결 완료 시 getPresignedDownloadUrl(fileKey) 호출로 변경
-    res.status(200).json({ url: fileRecord.url })
+    // res.status(200).json({ url: fileRecord.url })
         
 }
